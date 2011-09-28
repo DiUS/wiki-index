3c9d07d4d826fc7fe0462704a42989e2ba20fc75
Tal Rotbart <redbeard@gmail.com>
2011-09-28 13:55:47 +1000

    More clean up after conversion from Mirah

--- src/test/java/com/springsense/wikiindex/ArticleTest.java
+++ src/test/java/com/springsense/wikiindex/ArticleTest.java
@@ -1,31 +1,31 @@
-// Generated from article_test.mirah
 package com.springsense.wikiindex;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
-public class ArticleTest extends java.lang.Object {
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArticleTest {
 	private int id;
-	private java.lang.String title;
-	private java.lang.String articleWikitext;
-	private com.springsense.wikiindex.Article article;
+	private String title;
+	private String articleWikitext;
+	private Article article;
 
-	@org.junit.Before()
+	@Before()
 	public void setup() throws Exception {
-		this.id = -1;
-		this.title = "Fake title";
-		this.articleWikitext = org.apache.commons.io.IOUtils.toString(this
-				.getClass().getClassLoader()
+		id = -1;
+		title = "Fake title";
+		articleWikitext = IOUtils.toString(getClass().getClassLoader()
 				.getResourceAsStream("test-article.wikitext"));
-		this.article = new com.springsense.wikiindex.Article(this.id,
-				this.title, this.articleWikitext);
+		article = new Article(id, title, articleWikitext);
 	}
 
-	@org.junit.Test()
-	public void article_should_instantiate_correctly() {
-		org.junit.Assert.assertThat(new java.lang.Integer(this.article.id()),
-				org.hamcrest.Matchers.equalTo(new java.lang.Integer(this.id)));
-		org.junit.Assert.assertThat(this.article.title(),
-				org.hamcrest.Matchers.equalTo(this.title));
-		org.junit.Assert.assertThat(this.article.wikitext(),
-				org.hamcrest.Matchers.equalTo(this.articleWikitext));
+	@Test()
+	public void articleShouldInstantiateCorrectly() {
+		assertThat(article.id(), equalTo(new Integer(id)));
+		assertThat(article.title(), equalTo(title));
+		assertThat(article.wikitext(), equalTo(articleWikitext));
 	}
 }
